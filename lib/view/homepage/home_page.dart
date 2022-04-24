4d3b6dcd493e0394410378816acf48aed0508e15
NaufalFiraas <naufalfiraas09@gmail.com>
2022-04-24 15:07:10 +0700

    styling awal secara keseluruhan untuk sliver appbar

---
+++ lib/view/homepage/home_page.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  final List<String> dropdownMenus = [
+    'Belanja',
+    'Kerja',
+    'Belajar',
+    'Travelling',
+    'Produktivitas',
+    'Hobi',
+    'Kuliner',
+    'Lainnya...',
+    'Semua',
+  ];
+
+  List<DropdownMenuItem<String>> dropdownMenuBuilder(List<String> menus) {
+    List<DropdownMenuItem<String>> items = [];
+    for (String element in menus) {
+      items.add(
+        DropdownMenuItem<String>(
+          value: element,
+          child: Text(
+            element,
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w400,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      );
+    }
+    return items;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      endDrawer: const Drawer(),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(15),
+                bottomRight: Radius.circular(15),
+              ),
+            ),
+            pinned: false,
+            floating: true,
+            snap: false,
+            expandedHeight: 150,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [
+                      Color(0xFF309CFF),
+                      Color(0xFF044D90),
+                    ],
+                  ),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(15),
+                    bottomRight: Radius.circular(15),
+                  ),
+                ),
+                child: SafeArea(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Senin',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const Text(
+                        '25-04-2022',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 12.5,
+                      ),
+                      DropdownButton(
+                        items: dropdownMenuBuilder(dropdownMenus),
+                        onChanged: (value) {},
+                        value: 'Semua',
+                        isExpanded: true,
+                        dropdownColor: const Color(0xFF309CFF),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
