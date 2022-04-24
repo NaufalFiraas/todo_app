33662bb0c4409f1816da725c1dea1eacfcaadfcb
NaufalFiraas <naufalfiraas09@gmail.com>
2022-04-24 17:04:50 +0700

    styling awal komponen todo container

--- lib/view/homepage/home_page.dart
+++ lib/view/homepage/home_page.dart
@@ -48,7 +48,7 @@ class HomePage extends StatelessWidget {
             ),
             pinned: false,
             floating: true,
-            snap: false,
+            snap: true,
             expandedHeight: 150,
             flexibleSpace: FlexibleSpaceBar(
               background: Container(
@@ -102,6 +102,177 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
               ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return Dismissible(
+                  key: UniqueKey(),
+                  direction: DismissDirection.endToStart,
+                  confirmDismiss: (_) {
+                    return showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          title: const Text(
+                            'Hapus',
+                            style: TextStyle(
+                              color: Color(0xFF309CFF),
+                            ),
+                          ),
+                          content: const Text(
+                            'Yakin Hapus?',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black54,
+                            ),
+                          ),
+                          actions: [
+                            TextButton(
+                              child: const Text(
+                                'Ya',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color(0xFF309CFF),
+                                ),
+                              ),
+                              onPressed: () {},
+                            ),
+                            TextButton(
+                              child: const Text(
+                                'Batal',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color(0xFF309CFF),
+                                ),
+                              ),
+                              onPressed: () {
+                                Navigator.pop(context, false);
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    margin: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: const [
+                        BoxShadow(
+                          blurRadius: 10,
+                          spreadRadius: 3,
+                          color: Colors.black26,
+                          offset: Offset(0, 0),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.fromLTRB(15, 2, 15, 2),
+                          width: double.infinity,
+                          decoration: const BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Color(0xFF309CFF),
+                                Color(0xFF044D90),
+                              ],
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                            ),
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(15),
+                              topRight: Radius.circular(15),
+                            ),
+                          ),
+                          child: const Text(
+                            'Hari Ini',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        ListTile(
+                          leading: Checkbox(
+                            shape: const StadiumBorder(),
+                            activeColor: Colors.amber,
+                            value: true,
+                            onChanged: (value) {},
+                          ),
+                          title: const Text(
+                            'Judul',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400,
+                              color: Color(0xFF309CFF),
+                            ),
+                          ),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const SizedBox(
+                                height: 3,
+                              ),
+                              const Text(
+                                'Deskripsi',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color(0xFF787875),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 3,
+                              ),
+                              const Text(
+                                '25-04-2022',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                  color: Color(0xFF787875),
+                                ),
+                              ),
+                              const Text(
+                                '17:00',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                  color: Color(0xFF787875),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 3,
+                              ),
+                              const Text(
+                                'Semua',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                  color: Color(0xFF787875),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
             ),
           ),
         ],
