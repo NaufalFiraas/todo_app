27237516d48d8dd7c41d6a2c77bfb8fe7a9acbb5
naufal <naufalfiraas09@gmail.com>
2022-06-08 12:57:43 +0700

    menambahkan method showScheduledNotif

--- lib/data/providers/notification_provider.dart
+++ lib/data/providers/notification_provider.dart
@@ -1,14 +1,17 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/timezone.dart' as tz;
 
 class NotificationProvider {
   late final FlutterLocalNotificationsPlugin notification;
 
-  NotificationProvider([FlutterLocalNotificationsPlugin? optionalNotification]) {
+  NotificationProvider(
+      [FlutterLocalNotificationsPlugin? optionalNotification]) {
     notification = optionalNotification ?? FlutterLocalNotificationsPlugin();
   }
 
   Future<void> init() async {
-    AndroidInitializationSettings androidSettings = AndroidInitializationSettings('@mipmap/ic_launcher');
+    AndroidInitializationSettings androidSettings =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
     IOSInitializationSettings iOSSettings = IOSInitializationSettings();
 
     InitializationSettings settings = InitializationSettings(
@@ -30,4 +33,17 @@ class NotificationProvider {
     );
   }
 
+  Future<void> showScheduledNotif(
+      int id, String title, String body, DateTime time) async {
+    notification.zonedSchedule(
+      id,
+      title,
+      body,
+      tz.TZDateTime.from(time, tz.getLocation('Asia/Jakarta')),
+      notificationDetails(),
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+      androidAllowWhileIdle: true,
+    );
+  }
 }
